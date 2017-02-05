1bc7ff43930a15d9d9c077310eff83bdb3722d12
Laurence344 <laurence.namysl@polymtl.ca>
2017-02-04 19:40:03 -0500

    Add files via upload
    j'ai essayé de créer la table de connections à partir d'un échantillon réduit

--- libraries/p5.dom.js
+++ libraries/p5.dom.js
@@ -1,4 +1,4 @@
-/*! p5.dom.js v0.3.1 Jan 3, 2017 */
+/*! p5.dom.js v0.2.13 Oct 1, 2016 */
 /**
  * <p>The web is much more than just canvas and p5.dom makes it easy to interact
  * with other HTML5 objects, including text, hyperlink, image, input, video,
@@ -33,7 +33,6 @@
   else
     factory(root['p5']);
 }(this, function (p5) {
-
 // =============================================================================
 //                         p5 additions
 // =============================================================================
@@ -292,7 +291,7 @@
   });
 
   /**
-   * Creates an &lt;img&gt; element in the DOM with given src and
+   * Creates an &lt;img /&gt; element in the DOM with given src and
    * alternate text.
    * Appends to the container node if one is specified, otherwise
    * appends to body.
@@ -315,8 +314,8 @@
     var args = arguments;
     var self;
     var setAttrs = function(){
-      self.width = elt.offsetWidth || elt.width;
-      self.height = elt.offsetHeight || elt.height;
+      self.width = elt.offsetWidth;
+      self.height = elt.offsetHeight;
       if (args.length > 1 && typeof args[1] === 'function'){
         self.fn = args[1];
         self.fn();
@@ -721,8 +720,7 @@
    * appends to body.
    *
    * @method createInput
-   * @param {Number} [value] default value of the input box
-   * @param {String} [type] type of text, ie text, password etc. Defaults to text
+   * @param  {Number} [value] default value of the input box
    * @return {Object/p5.Element} pointer to p5.Element holding created node
    * @example
    * <div class='norender'><code>
@@ -737,9 +735,9 @@
    *
    * </code></div>
    */
-  p5.prototype.createInput = function(value, type) {
+  p5.prototype.createInput = function(value) {
     var elt = document.createElement('input');
-    elt.type = type ? type : 'text';
+    elt.type = 'text';
     if (value) elt.value = value;
     return addElement(elt, this);
   };
@@ -752,28 +750,6 @@
    * @param  {Function} [callback] callback function for when a file loaded
    * @param  {String} [multiple] optional to allow multiple files selected
    * @return {Object/p5.Element} pointer to p5.Element holding created DOM element
-   * @example
-   * var input;
-   * var img;
-   *
-   * function setup() {
-   *   input = createFileInput(handleFile);
-   *   input.position(0, 0);
-   * }
-   *
-   * function draw() {
-   *   if (img) {
-   *     image(img, 0, 0, width, height);
-   *   }
-   * }
-   *
-   * function handleFile(file) {
-   *   print(file);
-   *   if (file.type === 'image') {
-   *     img = createImg(file.data);
-   *     img.hide();
-   *   }
-   * }
    */
   p5.prototype.createFileInput = function(callback, multiple) {
 
@@ -1023,13 +999,8 @@
     // set width and height onload metadata
     elt.addEventListener('loadedmetadata', function() {
       elt.play();
-      if (elt.width) {
-        c.width = elt.videoWidth = elt.width;
-        c.height = elt.videoHeight = elt.height;
-      } else {
-        c.width = c.elt.width = elt.videoWidth;
-        c.height = c.elt.height = elt.videoHeight;
-      }
+      c.width = c.elt.width = elt.videoWidth;
+      c.height = c.elt.height = elt.videoHeight;
       c.loadedmetadata = true;
     });
     return c;
@@ -1132,7 +1103,7 @@
    * </code></div>
    */
   p5.Element.prototype.child = function(c) {
-    if (typeof c === 'undefined'){
+    if (c === null){
       return this.elt.childNodes
     }
     if (typeof c === 'string') {
@@ -1203,35 +1174,26 @@
   /**
    *
    * If an argument is given, sets the inner HTML of the element,
-   * replacing any existing html. If true is included as a second
-   * argument, html is appended instead of replacing existing html.
-   * If no arguments are given, returns
+   * replacing any existing html. If no arguments are given, returns
    * the inner HTML of the element.
    *
    * @for p5.Element
    * @method html
    * @param  {String} [html] the HTML to be placed inside the element
-   * @param  {boolean} [append] whether to append HTML to existing
    * @return {Object/p5.Element|String}
    * @example
    * <div class='norender'><code>
    * var div = createDiv('').size(100,100);
+   * div.style('background-color','orange');
    * div.html('hi');
    * </code></div>
-   * <div class='norender'><code>
-   * var div = createDiv('Hello ').size(100,100);
-   * div.html('World', true);
-   * </code></div>
    */
-  p5.Element.prototype.html = function() {
-    if (arguments.length === 0) {
-      return this.elt.innerHTML;
-    } else if (arguments[1]) {
-      this.elt.innerHTML += arguments[0];
+  p5.Element.prototype.html = function(html) {
+    if (typeof html !== 'undefined') {
+      this.elt.innerHTML = html;
       return this;
     } else {
-      this.elt.innerHTML = arguments[0];
-      return this;
+      return this.elt.innerHTML;
     }
   };
 
@@ -1860,10 +1822,8 @@
   p5.MediaElement.prototype.get = function(x, y, w, h){
     if (this.loadedmetadata) { // wait for metadata
       return p5.Renderer2D.prototype.get.call(this, x, y, w, h);
-    } else if (typeof x === 'undefined') {
+    } else if (!x) {
       return new p5.Image(1, 1);
-    } else if (w > 1) {
-      return new p5.Image(x, y, w, h);
     } else {
       return [0, 0, 0, 255];
     }
---
+++ sketch.js
@@ -0,0 +1,42 @@
+var connections = [];
+var n = 0;
+var table;
+
+function preload() {
+  table = loadTable("stop_times_reduit.csv", "csv", "header");
+}
+
+
+
+function setup() {
+  createCanvas(600,900);
+  background(200);
+  tableconnection();
+  textSize(20);
+  fill(100,100,200);
+  text(table.getRowCount() + " total rows in table",10,15);
+  text(table.getColumnCount() + " total columns in table",10,40);
+  for (var r = 0 ; r < connections.length ; r++){
+  textSize(7);
+  fill(200,250,100);
+  text(connections[r],200,10*(r+5));
+  }
+
+}
+
+function draw() {
+  
+}
+
+function tableconnection (){
+  for(var i = 1; i< table.getRowCount(); i++){
+    
+    var trip1 = float(table.getString(i, 0));
+    var trip2 = float(table.getString(i-1, 0))
+  if (trip1 == trip2){
+  var time = float(table.getString(i, 1))-float(table.getString((i-1), 1));
+    connections[n]=[table.getString((i-1), 1),table.getString((i-1), 3),table.getString(i, 3),time,false,{},table.getString(i, 0),'route_id'];
+      n=n+1;
+    }
+  }
+}
